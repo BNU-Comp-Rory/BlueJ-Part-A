57c2135dff52aab12927b0414bed72c8a703c02e
Roars-Tech <roryaguilfoyle@outlook.com>
2020-10-13 19:47:18 +0100

    Ticket creation

--- app02-ticketmachine/Ticket.java
+++ app02-ticketmachine/Ticket.java
@@ -1,5 +1,4 @@
 import java.util.Date;
-import java.text.SimpleDateFormat;
 
 /**
  * Tickets and Prices to locations that the TicketMachine can print.
@@ -18,8 +17,14 @@ public class Ticket
   private Date issueDateTime;
     
   private int price;
-    
-   
+  
+  private Ticket aylesburyTicket;
+  private Ticket amershamTicket;
+  private Ticket highWycombeTicket;
+  
+  //public static final Ticket AYLESBURY_TICKET = new Ticket("Aylesbury", 200);
+  //public static final Ticket AMERSHAM_TICKET = new Ticket("Amersham", 300);
+  //public static final Ticket HIGHWYCOMBE_TICKET = new Ticket("High Wycombe", 330);
   /**
    * Setting the destination, price for the Ticket. 
    * The date will populate to the current systems date.
@@ -28,15 +33,24 @@ public class Ticket
   {
     this.destination = destination;
     this.price = price;
+    
         
     issueDateTime = new Date();
   }
+  
+  // /**
+   // * 
+   // */
+  // public void createTicket ()
+  // {
+      // Ticket aylesbury = AYLESBURY_TICKET;
+  // }
     
   /**
   *Print method for the Ticket.
   *It will display the Destination, Price and Date Issued.
   */
-  public void print()
+  public void printTicket()
   {   
      System.out.println("################");
      System.out.println("     Ticket     ");
@@ -45,4 +59,21 @@ public class Ticket
      System.out.println("Date Issued : " + issueDateTime);
      System.out.println("################");
   }   
+  
+  /**
+   * 
+   */
+  public String getDestination ()
+  {
+   return destination;
+  }
+  
+  /**
+   * 
+   */
+  public int price()
+  {
+      return price;
+  }
 }
+
