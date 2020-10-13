a786c52df6f8e4715b0aee8492b13b5a0bbd46b1
Roars-Tech <roryaguilfoyle@outlook.com>
2020-10-13 14:02:10 +0100

    Ticket Created
    inputs destination and cost
    only using int at the moment will look into enum

--- app02-ticketmachine/Ticket.java
+++ app02-ticketmachine/Ticket.java
@@ -1,33 +1,48 @@
+import java.util.Date;
+import java.text.SimpleDateFormat;
 
 /**
  * Tickets and Prices to locations that the TicketMachine can print.
  *
  * @ Author  Rory Guilfoyle
  * @ Version 07.10.2020
+ * 
+ * @ Modified 13.10.2020
  */
 public class Ticket
 {
-    // instance variables - replace the example below with your own
-    // public Get.location ;
-
-    /**
-     * Constructor for objects of class Ticket
-     */
-    public Ticket()
-    {
-        //initialise instance variables
+  // Attributes
+    
+  private String destination;
+    
+  private Date issueDateTime;
+    
+  private int price;
+    
+   
+  /**
+   * Setting the destination, price for the Ticket. 
+   * The date will populate to the current systems date.
+   */
+  public Ticket(String destination, int price)
+  {
+    this.destination = destination;
+    this.price = price;
         
-    }
-
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    // public Get location ()
-    {
-        // put your code here
-       
-    }
+    issueDateTime = new Date();
+  }
+    
+  /**
+  *Print method for the Ticket.
+  *It will display the Destination, Price and Date Issued.
+  */
+  public void print()
+  {   
+     System.out.println("################");
+     System.out.println("     Ticket     ");
+     System.out.println("Destination : " + destination);
+     System.out.println("Price : " + price);
+     System.out.println("Date Issued : " + issueDateTime);
+     System.out.println("################");
+  }   
 }
